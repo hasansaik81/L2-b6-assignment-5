46a5d3b73486a12b4fbd608d267143222544e466
Hasan Saik <hasansaik81@gmail.com>
2026-04-30 21:22:13 +0600

    category route

--- src/modules/Category/category.controller.ts
+++ src/modules/Category/category.controller.ts
@@ -1,3 +1,181 @@
+import { NextFunction, Request, Response } from "express";
+import { CategoryService } from "./category.service";
+import sendResponse from "../../utils/sendResponse";
+import { prisma } from "../../lib/prisma";
+
+const getAllCategory=async (req:Request,res:Response ,next:NextFunction)=>{
+  try{
+    const result =await CategoryService.getAllCategoryIntoDB(req.user?.id);
+    sendResponse(res,{
+      statusCode:201,
+      success:true,
+      message:"Tutor Category retrived Successfully.",
+      data:result,
+    });
+  }catch(error:any){
+    next(error)
+  };
+};
+
+const getPublicAllCategory=async(req:Request,res:Response,next:NextFunction)=>{
+  try{
+    const result =await CategoryService.getPublicAllCategoryIntoDB();
+  sendResponse(res,{
+    statusCode:201,
+    success:true,
+    message:"Public Categories retrieved successfully.",
+    data:result,
+  })
+  }catch(error:any){
+    next(error)
+  }
+};
+
+
+const getPublicSingleCategory=async(req:Request,res:Response,next:NextFunction)=>{
+  try{
+    const result =await CategoryService.getPublicSingleCategoryIntoDB(req.params?.id as string);
+    sendResponse(res,{
+      statusCode:201,
+      success:true,
+      message:"Public Category retrieved successfully.",
+      data:result,
+    });
+  }catch(error:any){
+    next(error)
+  }
+};
+
+//  const getAllSubjects = async (req: Request,res: Response,next: NextFunction
+// ) => {
+//   try {
+//     console.log("SUBJECT API HIT");
+
+//     const subjects = await prisma.subject.findMany({
+//       include: {
+//         category: true,
+//       },
+//       orderBy: {
+//         createdAt: "desc",
+//       },
+//     });
+
+//     sendResponse(res, {
+//       statusCode: 200,
+//       success: true,
+//       message: "Subjects fetched successfully",
+//       data: subjects,
+//     });
+//   } catch (error: any) {
+//     next(error);
+//   }
+// };
+
+
+
+const createCategory=async(req:Request, res:Response ,next:NextFunction)=>{
+ 
+    try{
+      const result = await CategoryService.createCategoryIntoDB(
+        req.body,
+        req.user?.id,
+      );
+      sendResponse(res,{
+        statusCode:201,
+        success:true,
+        message:"Tutor Category  Created Successfully ",
+        data:result,
+
+      });
+    }catch (error:any){
+      
+      next()
+    }
+};
+
+
+const getSingleCategory=async(req:Request,res:Response,next:NextFunction)=>{
+  try{
+  
+    const result=await CategoryService.getSingleCategoryIntoDB(req.params?.id as string);
+    sendResponse(res,{
+      statusCode:201,
+      success:true,
+      message:"Tutor Category retrived Successfully.",
+      data:result,
+    });
+  }catch(error:any){
+  next(error)
+  }
+};
+
+
+
+
+
+// const createSubject= async(req:Request,res:Response,next:NextFunction)=>{
+//     try{
+//     const result=await CategoryService.createSubject(req.body)
+//     sendResponse(res,{
+//       statusCode:201,
+//       success:true,
+//       message:"subject create successfully",
+//       data:result
+//     })
+
+//     }catch (error:any){
+//         next(error)
+//     }
+// }
+
+
+
+
+
+// const updateSubject= async(req:Request,res:Response,next:NextFunction)=>{
+//     try{
+//     const result=await CategoryService.updateSubject(req.body,req.params?.id as string)
+//     sendResponse(res,{
+//       statusCode:201,
+//       success:true,
+//       message:"subject updated successfully",
+//       data:result
+//     })
+//     }catch (error:any){
+//     next(error)
+//     }
+// }
+
+// const deleteSubject= async(req:Request,res:Response,next:NextFunction)=>{
+//     try{
+//     const result=await CategoryService.deleteSubject(req.params?.id as string)
+//     sendResponse(res,{
+//       statusCode:201,
+//       success:true,
+//       message:"subject deleted successfully",
+//       data:result
+//     })
+//     }catch (error:any){
+//      next(error)
+//     }
+// }
+
+
+
 export const CategoryController = {
-    // Add controller methods here
-    };
+    
+    getAllCategory,
+    getSingleCategory,
+    getPublicAllCategory,
+    getPublicSingleCategory,
+    createCategory,
+  //   createSubject,
+  //  getAllSubjects,
+  //   updateSubject,
+  //   deleteSubject
+};
+    
+
+
+
+
--- src/modules/Category/category.route.ts
+++ src/modules/Category/category.route.ts
@@ -1,5 +1,20 @@
 import express from 'express';
+import auth, { UserRole } from '../../middlewares/auth';
+import { CategoryController } from './category.controller';
 
 const router = express.Router();
+router.get("/public",CategoryController.getPublicAllCategory);
+
+router.get("/public/:id",CategoryController.getPublicSingleCategory);
+
+router.get("/",auth(UserRole.tutor),CategoryController.getAllCategory);
+
+router.post("/",auth(UserRole.tutor,UserRole.student),CategoryController.createCategory)
+
+router.get("/:id",auth(UserRole.student),CategoryController.getSingleCategory);
+
+
+
 
 export const CategoryRoutes = router;
+
