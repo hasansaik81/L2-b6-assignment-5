67e4f7f74aa630662251cdb92303d484b902369b
Hasan Saik <hasansaik81@gmail.com>
2026-04-30 21:20:21 +0600

    category service

--- src/modules/Category/category.service.ts
+++ src/modules/Category/category.service.ts
@@ -1,3 +1,108 @@
+
+
+import { prisma } from "../../lib/prisma"
+
+
+const getAllCategoryIntoDB=async(userId:string)=>{
+
+
+  const tutorProfile=await prisma.tutorProfiles.findUnique({
+    where:{
+      tutorId:userId,
+    },
+  });
+  if(!tutorProfile){
+    throw new Error ("Tutor Profile not found!!")
+  }
+  const result =await prisma.category.findMany({
+    where:{
+      tutorId:tutorProfile.id,
+    },
+    include:{
+      tutor:true
+    },
+  });
+
+  return result;
+};
+
+
+const getPublicAllCategoryIntoDB = async () => {
+  const result = await prisma.category.findMany({
+    include: {
+      tutor: {
+        include: {
+          user:true
+        },
+      },
+     
+    },
+  });
+
+  return result;
+};
+
+
+
+
+const getPublicSingleCategoryIntoDB = async (categoryId: string) => {
+  return await prisma.category.findUnique({
+    where: {
+      id: categoryId,
+    },
+    include: {
+      tutor: {
+        include: {
+          user: true,
+        },
+      },
+    },
+  });
+};
+
+
+const createCategoryIntoDB=async(payLoad:any,userId:string)=>{
+
+  const tutorProfile=await prisma.tutorProfiles.findUnique({
+    where:{
+    tutorId:userId,
+    },
+  });
+  if(!tutorProfile){
+    throw new Error ("Tutor Profile not found");
+  }
+  const result= await prisma.category.create({
+    data:{...payLoad,tutorId:tutorProfile.id},
+  });
+  return result;
+};
+
+//  tutorId:user
+// userId: user.id
+
+
+
+const getSingleCategoryIntoDB = async (tutorId: string) => {
+  const result = await prisma.tutorProfiles.findUnique({
+    where: {
+      id: tutorId,
+    },
+    
+  });
+
+  return result;
+};
+
+
+
+
 export const CategoryService = {
-    // Add service methods here
-    };
+  createCategoryIntoDB,
+  getAllCategoryIntoDB,
+    getSingleCategoryIntoDB,
+    getPublicAllCategoryIntoDB,
+    getPublicSingleCategoryIntoDB,
+   
+   
+};
+  
